249090be4e3a9a74b048d32e5f80ae1332d46ad5
dbaxime <pierre.ranucci@gmail.com>
2015-08-10 19:12:13 +0200

    __

---
+++ includes/libft.h
@@ -0,0 +1,9 @@
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/*            ecriture             */
+void	ft_putchar(char c);
+
+
+#endif
---
+++ srcs/ft_putstr.c
@@ -0,0 +1,7 @@
+#include <unsitd.h>
+#include "libft.h"
+
+void	ft_putstr(const char *s)
+{
+	write(1, s, ft_strlen(s));
+}
