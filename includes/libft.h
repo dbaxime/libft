6e505eef76130d661483e0dabe1c8dd71e1369d8
dbaxime <pierre.ranucci@gmail.com>
2015-08-11 19:16:30 +0200

    __

--- includes/libft.h
+++ includes/libft.h
@@ -1,9 +1,21 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
+# include <string.h>
+# include <unistd.h>
 /*            ecriture             */
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
+void	ft_putendl(char const *s);
+
+/*               traitement de chaines*/
+size_t	ft_strlen(const char *s);
+
+/*               traitement caracteres*/
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 
 #endif
